2c359e484b455a6adabcfcd416d0d29f6f642ee9
Antonio Sala <59183374+Antho137@users.noreply.github.com>
2025-03-24 14:10:42 +0100

    Update experiences.tsx

--- src/app/components/experiences.tsx
+++ src/app/components/experiences.tsx
@@ -4,7 +4,7 @@ const Experiences = () => {
     return (
         <div className='bg-blue-200 flex flex-col justify-center content-center text-center py-10 px-5' id='my-experiences'>
             <h2 className='text-3xl text-orange-600 mb-5'>Experiences</h2>
-            <div className='md:mx-auto md:w-[70%] lg:w-[60%] xl:w-[55%]'>
+            <div className='text-lg md:mx-auto md:w-[70%] lg:w-[60%] xl:w-[55%]'>
                 <div className='mb-3'>
                     <p>My experience includes <a className='text-blue-500' href="https://www.w3schools.com/whatis/">developing websites</a> for small businesses, non-profit organizations and personal projects. My strength is the ability to work independently. I am able to design every element of the page from start to finish without a constant supervision, following <a className='text-blue-500' href="https://careerfoundry.com/en/blog/web-development/8-biggest-trends">web industry strends</a>.
                     </p>
